d2c1b962262429804b565bd674610291b303362b
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 07:06:33 -0700

    Data table for by country completed..

--- src/app/country/components/country-page/country-page.component.ts
+++ src/app/country/components/country-page/country-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.css']
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+covidData: any;
 
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    this.dataService.getSummaryData().subscribe((data: any) => {
+      this.covidData = data;
+    });
+  }
 }
--- src/app/country/components/data-table/data-table.component.ts
+++ src/app/country/components/data-table/data-table.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent {
+export class DataTableComponent implements OnInit, OnChanges {
+@Input() covidData: any;
+countries: any[] = []
+  constructor(){}
 
+  ngOnInit(): void {
+    
+  }
+  ngOnChanges(): void {
+  this.countries = this.covidData?.Countries;
+  console.log('data datatbale', this.countries);
+  
+}
 }
