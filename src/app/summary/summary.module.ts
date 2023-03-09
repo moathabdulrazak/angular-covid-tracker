216b7cb9bcd4b155043d02baae0fc33bd71b6a96
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 08:13:01 -0700

    learning how to reuse components

--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -5,17 +5,21 @@ import { SummaryRoutingModule } from './summary-routing.module';
 import { SummaryPageComponent } from './components/summary-page/summary-page.component';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
 import { TopCountriesComponent } from './components/top-countries/top-countries.component';
+import { ConfirmedByCountryComponent } from './components/confirmed-by-country/confirmed-by-country.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     SummaryPageComponent,
     SummaryCardsComponent,
-    TopCountriesComponent
+    TopCountriesComponent,
+    ConfirmedByCountryComponent
   ],
   imports: [
     CommonModule,
-    SummaryRoutingModule
+    SummaryRoutingModule,
+    SharedModule
   ]
 })
 export class SummaryModule { }
