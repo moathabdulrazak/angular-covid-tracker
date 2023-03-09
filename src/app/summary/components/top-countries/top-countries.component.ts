6da17012b9fc5d1ff2de760f58d477ebd2fc3850
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 04:37:45 -0700

    first set of data complete. new compoents built

---
+++ src/app/summary/components/top-countries/top-countries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-countries',
+  templateUrl: './top-countries.component.html',
+  styleUrls: ['./top-countries.component.css']
+})
+export class TopCountriesComponent {
+
+}
--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SummaryRoutingModule } from './summary-routing.module';
 import { SummaryPageComponent } from './components/summary-page/summary-page.component';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
+import { TopCountriesComponent } from './components/top-countries/top-countries.component';
 
 
 @NgModule({
   declarations: [
     SummaryPageComponent,
-    SummaryCardsComponent
+    SummaryCardsComponent,
+    TopCountriesComponent
   ],
   imports: [
     CommonModule,
