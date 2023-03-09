2ed9a9ce9c40b0efac0e0aeda38b27db58ef2868
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 00:13:41 -0700

    url routing is working.

--- src/app/live/live-routing.module.ts
+++ src/app/live/live-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LivePageComponent } from './components/live-page/live-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LivePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/summary/summary-routing.module.ts
+++ src/app/summary/summary-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SummaryPageComponent } from './components/summary-page/summary-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SummaryPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
