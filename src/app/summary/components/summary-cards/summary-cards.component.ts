2b7ba5f6fb5d6d12f53ad86f1c9af56da9deacb7
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 04:25:37 -0700

    first data pulled and displaying

--- src/app/summary/components/summary-cards/summary-cards.component.ts
+++ src/app/summary/components/summary-cards/summary-cards.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-summary-cards',
   templateUrl: './summary-cards.component.html',
   styleUrls: ['./summary-cards.component.css']
 })
-export class SummaryCardsComponent {
+export class SummaryCardsComponent implements OnInit, OnChanges {
+@Input() covidData: any;
+summaryData: any = {};
+  constructor(){}
 
+ngOnInit(): void {
+  
+}
+
+ngOnChanges(): void {
+  this.summaryData = this.covidData.Global;
+  console.log(this.summaryData);
+  
+}
 }
