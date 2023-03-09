eb09b6bf033d2eb7b653b587d7f6d11722724bc3
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 06:43:09 -0700

    building mock table component for search.

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CountryRoutingModule } from './country-routing.module';
 import { CountryPageComponent } from './components/country-page/country-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 
 @NgModule({
   declarations: [
-    CountryPageComponent
+    CountryPageComponent,
+    DataTableComponent
   ],
   imports: [
     CommonModule,
