e5776b2022f7fcbae4e37e4b3dacf061875851af
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-09 17:09:55 -0700

    chart on summary page hardcoded data

--- src/app/summary/components/country-chart/country-chart.component.ts
+++ src/app/summary/components/country-chart/country-chart.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts/lib/base-chart.directive';
 
 @Component({
   selector: 'app-country-chart',
   templateUrl: './country-chart.component.html',
   styleUrls: ['./country-chart.component.css']
 })
-export class CountryChartComponent {
+export class CountryChartComponent implements OnInit {
 
+barChartData: ChartDataSets[] = [
+  {
+    data: [65, 59, 80]
+  }
+];
+barChartLabels: Label[] = ['USA', 'UK', 'Brazil']
+barChartOptions: ChartOptions = {
+  responsive: true
+}
+
+barChartType: ChartType = 'bar'
+barChartLegend = true;
+
+constructor(){}
+
+
+  ngOnInit(): void {
+    
+  }
 }
